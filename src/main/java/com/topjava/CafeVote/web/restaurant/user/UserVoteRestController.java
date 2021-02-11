8bc575c64a3449be39798def2eadd48bee56bd5d
Dmitriy Bulanov <bulanovdms@gmail.com>
2021-02-11 15:19:31 +0300

    delete redundant method

--- src/main/java/com/topjava/CafeVote/web/restaurant/user/UserVoteRestController.java
+++ src/main/java/com/topjava/CafeVote/web/restaurant/user/UserVoteRestController.java
@@ -6,7 +6,6 @@ import com.topjava.CafeVote.service.MealService;
 import com.topjava.CafeVote.service.MenuService;
 import com.topjava.CafeVote.service.RestaurantService;
 import com.topjava.CafeVote.service.VoteService;
-import com.topjava.CafeVote.to.VoteTo;
 import com.topjava.CafeVote.web.restaurant.AbstractRestaurantRestController;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +18,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import springfox.documentation.annotations.ApiIgnore;
 
 import java.net.URI;
-import java.util.List;
 
 import static com.topjava.CafeVote.util.DateTimeUtil.getCurrentDate;
-import static com.topjava.CafeVote.util.DateTimeUtil.getCurrentTime;
-import static com.topjava.CafeVote.util.ValidationUtil.assureIdConsistent;
 
 @Slf4j
 @RestController
@@ -52,26 +48,19 @@ public class UserVoteRestController extends AbstractRestaurantRestController {
     public void updateVote(@PathVariable("restId") int restId, @RequestBody Vote vote, @PathVariable("id") int id,
                            @ApiIgnore @AuthenticationPrincipal AuthUser authUser) {
         log.info("user with id={} update vote for restaurant with id={}", authUser.id(), restId);
-        assureIdConsistent(vote, id);
-        voteService.update(vote, authUser.id(), restId, getCurrentDate(), getCurrentTime());
+        voteService.update(authUser.id(), restId);
     }
 
     @DeleteMapping("/{restId}/vote")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUserVote(@PathVariable("restId") int restId, @ApiIgnore @AuthenticationPrincipal AuthUser authUser) {
         log.info("delete vote by user with id={} by date={}", authUser.id(), getCurrentDate());
-        voteService.delete(authUser.id(), getCurrentDate(), getCurrentTime());
+        voteService.delete(authUser.id());
     }
 
     @GetMapping("/{restId}/vote/{id}")
     public Vote getTodayUserVote(@PathVariable("restId") int restId, @PathVariable("id") int id, @ApiIgnore @AuthenticationPrincipal AuthUser authUser) {
         log.info("get vote of user id={} for day {}", authUser.id(), getCurrentDate());
         return voteService.getVote(authUser.id(), getCurrentDate());
-    }
-
-    @GetMapping("/votes")
-    public List<VoteTo> getAllVotesForDay(@ApiIgnore @AuthenticationPrincipal AuthUser authUser) {
-        log.info("user with id={} get all his votes for current date", authUser.id());
-        return voteService.getAllForDateForUser(getCurrentDate(), authUser.id());
     }
 }
