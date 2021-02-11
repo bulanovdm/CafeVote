0ff47e7655cebca0ae0589190ff78f11b60e52fd
Dmitriy Bulanov <bulanovdms@gmail.com>
2021-02-11 15:16:12 +0300

    enable caching

--- src/main/java/com/topjava/CafeVote/CafeVoteApplication.java
+++ src/main/java/com/topjava/CafeVote/CafeVoteApplication.java
@@ -2,8 +2,9 @@ package com.topjava.CafeVote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
-
+@EnableCaching
 @SpringBootApplication
 public class CafeVoteApplication {
     public static void main(String[] args) {
